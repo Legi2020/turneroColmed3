b7ad0f0b1c65a66a87bb496b28e8fb2247daaefc
rootm3 <pablojesusalmada@hotmail.com>
2021-07-19 10:40:28 -0300

    se agrego interfaz + descarga en excel + se anulo acceso directo desde index

--- public/js/informes.js
+++ public/js/informes.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', obtenerArray);
-const table = document.getElementById('myTable');
   
   function obtenerArray(){
       const urlJSONArray = '/data.json';
@@ -8,8 +7,8 @@ const table = document.getElementById('myTable');
               return respuesta.json();
           })
           .then(resultado => {
-              console.log(resultado); //Hasta aca llega
-              crearTabla(resultado);
+              //console.log(resultado);
+              mostrarHTML(resultado);
           })
           .catch(error => {
               return error;
@@ -17,17 +16,58 @@ const table = document.getElementById('myTable');
   }
   
 
-  function crearTabla(data){
-      for (var i = 0; i < data.length; i++){
-          var row = `<tr>
-                          <td>${data[i].ultimo}</td>
-                          <td>${data.hoy}</td>
-                    </tr>`
-          table.innerHTML += row
+  function mostrarHTML({hoy, totalHistorico }) {
+    const contenido = document.getElementById('data');
+    let body = '';
 
-          console.log(resultado);
+    totalHistorico.forEach(data => {
+        const { numero, matricula, horario, escritorio, horario2 } = data;
+        
 
+        body += `<tr><td>${hoy}</td><td>${numero}</td><td>${matricula}</td><td>${escritorio}</td><td>${horario}</td><td>${horario2}</td></tr>`
 
-      }
+        contenido.innerHTML = body;
+    });
   }
-  
+
+  //Clase Exportar
+  class TableCSVExporter {
+    constructor (table, includeHeaders = true) {
+        this.table = table;
+        this.rows = Array.from(table.querySelectorAll("tr"));
+        if (!includeHeaders && this.rows[0].querySelectorAll("th").length) {
+            this.rows.shift();
+        }
+    }
+
+    convertirCSV () {
+        const lines = [];
+        const numCols = this._findLongestRowLength();
+
+        for (const row of this.rows) {
+            let line = "";
+            for (let i = 0; i < numCols; i++) {
+                if (row.children[i] !== undefined) {
+                    line += TableCSVExporter.parseCell(row.children[i]);
+                }
+                line += (i !== (numCols - 1)) ? "," : "";
+            }
+
+            lines.push(line);
+        }
+        return lines.join("\n");
+    }
+    _findLongestRowLength () {
+        return this.rows.reduce((l, row) => row.childElementCount > l ? row.childElementCount : l, 0);
+    }
+
+    static parseCell (tableCell) {
+        let parsedValue = tableCell.textContent;
+        //Reemplaza comillas dobles por simples
+        parsedValue = parsedValue.replace(/"/g, `""`);
+        //Si tiene algunos de los valores mencionados que los ponga con doble comilla
+        parsedValue = /[",\n]/.test(parsedValue) ? `"${parsedValue}"` : parsedValue;
+
+        return parsedValue;
+    }
+}
